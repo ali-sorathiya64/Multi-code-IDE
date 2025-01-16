4c066eaf5cf3a8f75119f3cac396da12b0606c65
ali-sorathiya64 <alisorathiya970@gmail.com>
2025-01-16 01:33:29 -0800

    add

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,10 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-// const conncetion_string ="mongodb+srv://andrew:garfield@project.pxx38.mongodb.net/code-IDE"
 
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL , {
+    await mongoose.connect(process.env.MONGO_URI , {
     
     });
     console.log("MongoDB connected");
@@ -13,6 +12,7 @@ const connectDB = async () => {
     console.error("Error connecting to MongoDB:", error.message);
     process.exit(1); // Exit process with failure
   }
-};
+
+}
 
 module.exports = connectDB;
